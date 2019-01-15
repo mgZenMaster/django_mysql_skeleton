1de7c26dc7e4609a6b86d8d13ae8d740243025e3
Christian Zintl <kontakt@mygig.de>
2019-01-15 21:30:55 +0100

    Fix migration creation and migration in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "Check if mysql is available."
 python /waitfordb.py
 
 if [ ! -f /.initdj ] ;then
-    python /app/manage.py makemigrations
+    python /app/manage.py makemigrations myapp
     python /app/manage.py migrate
 
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
